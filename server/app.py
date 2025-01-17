3a0661de5b13ccb490fa307044b59a41879342ea
Hass <hassmroueh@gmail.com>
2025-01-16 21:54:52 -0500

    building get api lab complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bake.to_dict() for bake in Bakery.query.all()]
+    
+    response = make_response(bakeries, 200)
+    return response
+    
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    response = make_response(bakery.to_dict(),200)
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    ser_baked_goods = [bg.to_dict() for bg in baked_goods]
+    return make_response(ser_baked_goods,200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return make_response(baked_good.to_dict(),200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
